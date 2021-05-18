d77f0e70fc7749979021844df1ccf5dedab15f63
gavin gu <guangyin_gu@sina.com>
2021-05-18 18:02:50 +0800

    Update ServiceDiscoveryListener.java

--- dynamic_sync_common/src/main/java/com/gavin/job/dynamic/sync/common/core/listener/ServiceDiscoveryListener.java
+++ dynamic_sync_common/src/main/java/com/gavin/job/dynamic/sync/common/core/listener/ServiceDiscoveryListener.java
@@ -3,7 +3,7 @@ package com.gavin.job.dynamic.sync.common.core.listener;
 import com.gavin.job.dynamic.sync.common.core.callback.INotifyCallback;
 
 /**
- * 服务发现主接口:需要实现该接口,监听master的通知行为
+ * 服务发现主接口:需要实现该接口,listen master的通知行为
  * 
  * @author gavin
  * 
