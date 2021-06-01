df2a97bf78ac5002933a416d506e9d1fc82f6755
gavin gu <guangyin_gu@sina.com>
2021-06-01 10:23:33 +0800

    Update annotation for holder

--- dynamic_sync_zkhandle/src/main/java/com/gavin/job/dynamic/sync/listener/zookeeper/notify/NodeHolder.java
+++ dynamic_sync_zkhandle/src/main/java/com/gavin/job/dynamic/sync/listener/zookeeper/notify/NodeHolder.java
@@ -2,8 +2,11 @@ package com.gavin.job.dynamic.sync.listener.zookeeper.notify;
 
 import java.util.LinkedHashMap;
 
+/**
+Global singleton holder for node instance
+**/
 public class NodeHolder {
-	protected static LinkedHashMap<String, Long> holder = new LinkedHashMap<String, Long>();
+	protected static LinkedHashMap<String, Long> holder = new LinkedHashMap<String, Long>();  //Node instance cache
 	private static final NodeHolder instance = new NodeHolder();
 
 	protected NodeHolder() {
